510b7ec3b559c9ec322a55abfff1009ed4a6cc51
ramachandraprasad77 <ramachandraprasad77@gmail.com>
2025-05-07 20:05:56 +0530

    netlify modified:   frontend/vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
+  server: {port: 5173,},
   plugins: [react(), tailwindcss()],
 });
